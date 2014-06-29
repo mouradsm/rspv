48b9e09965ce415787ed0f6b8e1b2cfab1894f32
Vagrant <vagrant@vagrant-ubuntu.vagrantup.com>
2014-06-29 03:29:15 -0300

    Testando

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 // Configurações ---
-//mongoose.connect('mongodb://diego:123456@kahana.mongohq.com:10041/app26894187');
-mongoose.connect('mongodb://localhost/rspv');
+mongoose.connect('mongodb://diego:123456@kahana.mongohq.com:10041/app26894187');
+//mongoose.connect('mongodb://localhost/rspv');
 var Presente = require('./app/models/presente');
 var Convidado = require('./app/models/convidado')
 var PresenteLista = require('./app/models/presentelista')
