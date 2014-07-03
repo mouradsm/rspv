d8aed241ca2fe8f29019ccf455db13943eca441b
Vagrant <vagrant@vagrant-ubuntu.vagrantup.com>
2014-07-02 22:12:07 -0300

    Testes do PostMark

--- server.js
+++ server.js
@@ -25,9 +25,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   next();
-}
-);
-
+  });
 
 
 var port = process.env.PORT || 3000; //configuração da porta
