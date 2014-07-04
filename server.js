878a84e2a62e42de90d41bcc21631f5ac28a8296
Vagrant <vagrant@vagrant-ubuntu.vagrantup.com>
2014-07-03 22:30:47 -0300

    Correções

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ var postmark = require("postmark")(process.env.POSTMARK_API_KEY)
 //var postmark = require("postmark")("8fef6ded-728c-4255-a146-f7ca0c425e70")
 
 // Configurações ---
-//mongoose.connect('mongodb://diego:123456@kahana.mongohq.com:10041/app26894187');
-mongoose.connect('mongodb://localhost/rspv');
+mongoose.connect('mongodb://diego:123456@kahana.mongohq.com:10041/app26894187');
+//mongoose.connect('mongodb://localhost/rspv');
 var Presente = require('./app/models/presente');
 var Convidado = require('./app/models/convidado')
 
